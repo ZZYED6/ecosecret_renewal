c51b43d95b3f0f92ae83ff0d233e8fd2668fb0c2
zzyeD <zzyedesign@gmail.com>
2024-09-11 18:36:11 +0900

    chore(ce_center) : 24.09.11 faq 버튼 안의 img +- 바뀌게 수정

--- js/sub_js/sub_cs_center.js
+++ js/sub_js/sub_cs_center.js
@@ -15,7 +15,7 @@ $(function () {
   $(".con_title > li").on("click", function (e) {
     e.preventDefault(); // 기본 동작 막기
 
-    var index = $(this).index() + 1; // 버튼의 인덱스 값 + 1
+    let index = $(this).index() + 1; // 버튼의 인덱스 값 + 1
 
     // 모든 버튼에서 active 클래스 제거
     $(".con_title > li").removeClass("active");
@@ -36,16 +36,41 @@ $(function () {
       let ts = $(this);
 
       // 페이지 로딩 시 모든 답변 숨기기
-      $("li>div", ts).hide();
+      $("li > div", ts).hide();
 
       $.each(ts, function (i, o) {
         $("button", o).on("click", tabmenu);
         function tabmenu() {
-          if ($(this).parent().next().is(":hidden")) {
+          // 클릭된 버튼 내 이미지 선택
+          let $clickedButton = $(this);
+          let $clickedImg = $("img", $clickedButton); // 클릭된 버튼의 이미지
+          let originalSrc = $clickedImg.data("original-src"); // 원래 이미지 src
+          let newSrc = $clickedImg.data("new-src"); // 변경할 이미지 src
+
+          // 답변 토글
+          if ($clickedButton.parent().next().is(":hidden")) {
+            // 다른 열려있는 div가 있을 경우 숨기기
             $("li > div:visible", o).hide();
-            $(this).parent().next().show();
+            // 현재 버튼의 div 열기
+            $clickedButton.parent().next().show();
+
+            // 모든 버튼의 이미지를 원래 상태로 복구
+            $("img", o).each(function () {
+              let $img = $(this);
+              $img.attr("src", $img.data("original-src")); // 원래 이미지로 변경
+            });
+
+            // 클릭된 버튼의 이미지를 새로운 이미지로 변경
+            $clickedImg.attr("src", newSrc); // 새로운 이미지로 변경
           } else {
-            $("li > div:visible", o).hide();
+            // 현재 버튼의 div 숨기기
+            $clickedButton.parent().next().hide();
+
+            // 모든 버튼의 이미지를 원래 상태로 복구
+            $("img", o).each(function () {
+              let $img = $(this);
+              $img.attr("src", $img.data("original-src")); // 원래 이미지로 변경
+            });
           }
         }
       });
