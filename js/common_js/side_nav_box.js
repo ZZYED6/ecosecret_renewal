4372d70e88493c1c84089c0ba87000525ee346cb
zzyeD <zzyedesign@gmail.com>
2024-09-16 15:32:13 +0900

    chore(common) : 24.09.16 사이드 메뉴 상품 카테고리 클릭이벤트 추가

--- js/common_js/side_nav_box.js
+++ js/common_js/side_nav_box.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const openIcon = document.querySelector(".snb_open-icon");
-const coloseIcon = document.querySelector(".snb_close-icon");
+const closeIcon = document.querySelector(".snb_close-icon");
 const sideNav = document.querySelector("#side_nav_box");
 
 function clickSideNav() {
@@ -16,7 +16,23 @@ function showSideNav() {
   sideNav.style.display = "flex";
 }
 
-coloseIcon.onclick = function () {
+closeIcon.onclick = function () {
   //.snb_close-icon 클릭하면  #side_nav_box 숨기기
   sideNav.style.display = "none";
 };
+
+document.addEventListener("DOMContentLoaded", function () {
+  const sidePrdList = document.querySelector(".side_prd_list > a");
+  const listTypes = document.querySelectorAll(".list_type");
+
+  sidePrdList.addEventListener("click", function (event) {
+    event.preventDefault(); // 기본 링크 클릭 동작 방지
+    listTypes.forEach(function (list) {
+      if (list.style.display === "block") {
+        list.style.display = "none"; // 닫기
+      } else {
+        list.style.display = "block"; // 열기
+      }
+    });
+  });
+});
