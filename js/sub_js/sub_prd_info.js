a0d397176274b1478fb5ea5c0cd8683112bacaca
zzyeD <zzyedesign@gmail.com>
2024-09-26 16:31:15 +0900

    chore(sub_product_info) : 24.09.26 상세 정보 코드 작성 chore : 24.09.26 모바일 반응형 - 메인비주얼 높이 수정, cart 페이지 버튼 수정

--- js/sub_js/sub_prd_info.js
+++ js/sub_js/sub_prd_info.js
@@ -50,4 +50,54 @@ $(document).ready(function () {
       img.attr("src", "../../img/icon/web_icons_svg/Heart-empty_gr.svg");
     }
   });
+  //상세정보
+  $.fn.extend({
+    detailInfo: function () {
+      let ts = $(this);
+
+      // 페이지 로딩 시 첫번째 내용 외 나머지 내용 안보이게 숨기기
+      $("li > div", ts).hide();
+      $("li:first-child > div", ts).show();
+
+      $.each(ts, function (i, o) {
+        $("button", o).on("click", tabmenu);
+        function tabmenu() {
+          // 클릭된 버튼 내 이미지 선택
+          let $clickedButton = $(this);
+          let $clickedImg = $("img", $clickedButton); // 클릭된 버튼의 이미지
+          let originalSrc = $clickedImg.data("original-src"); // 원래 이미지 src
+          let newSrc = $clickedImg.data("new-src"); // 변경할 이미지 src
+
+          // 답변 토글
+          if ($clickedButton.parent().next().is(":hidden")) {
+            // 다른 열려있는 div가 있을 경우 숨기기
+            // $("li > div:visible", o).hide();
+            // 현재 버튼의 div 열기
+            $clickedButton.parent().next().show();
+
+            // 모든 버튼의 이미지를 원래 상태로 복구
+            // $("img", o).each(function () {
+            //   let $img = $(this);
+            //   $img.attr("src", $img.data("original-src")); // 원래 이미지로 변경
+            // });
+
+            // 클릭된 버튼의 이미지를 새로운 이미지로 변경
+            $clickedImg.attr("src", newSrc); // 새로운 이미지로 변경
+          } else {
+            // 현재 버튼의 div 숨기기
+            $clickedButton.parent().next().hide();
+
+            // 모든 버튼의 이미지를 원래 상태로 복구
+            // $("img", o).each(function () {
+            //   let $img = $(this);
+            //   $img.attr("src", $img.data("original-src")); // 원래 이미지로 변경
+            // });
+            $clickedImg.attr("src", originalSrc); // 원래 이미지로 변경
+          }
+        }
+      });
+    },
+  });
+
+  $(".detail_info").detailInfo();
 });
