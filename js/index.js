b94020a0b633c6da33fef7ad5470f3a7e26b421f
zzyeD <zzyedesign@gmail.com>
2024-10-01 16:50:03 +0900

    chore : 24.10.01 이미지 폴더명 수정

--- js/index.js
+++ js/index.js
@@ -60,10 +60,10 @@ $(document).ready(function () {
     const img = $(this);
 
     // 이미지 소스를 변경
-    if (img.attr("src") === "./img/icon/web_icons_svg/heart-empty.svg") {
-      img.attr("src", "./img/icon/web_icons_svg/heart-full.svg");
+    if (img.attr("src") === "./img/icon/web_icons/heart-empty.svg") {
+      img.attr("src", "./img/icon/web_icons/heart-full.svg");
     } else {
-      img.attr("src", "./img/icon/web_icons_svg/heart-empty.svg");
+      img.attr("src", "./img/icon/web_icons/heart-empty.svg");
     }
   });
 });
